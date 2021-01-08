887a8a6487abd37e57dbbc70b3628f3159a1873a
Sarah Gilbert <sarahgilbert064@gmail.com>
2021-01-08 09:39:34 -0800

    update all of  MachineController for project by switching Child to Machine and Parent to Engineer

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -1,19 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
-using Template.Models;
+using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Template.Controllers
+namespace Factory.Controllers
 {
   // [Authorize]
-  public class ChildsController : Controller
+  public class MachinesController : Controller
   {
-    private readonly TemplateContext _db;
+    private readonly FactoryContext _db;
     // private readonly UserManager<ApplicationUser> _userManager;
 
-    public ChildsController(TemplateContext db)
+    public MachinesController(FactoryContext db)
     {
       // _userManager = userManager;
       _db = db;
@@ -21,25 +21,25 @@ namespace Template.Controllers
 
     public ActionResult Index()
     {
-      return View(_db.Childs.ToList());
+      return View(_db.Machines.ToList());
     }
 
     public ActionResult Create()
     {
-      ViewBag.ParentId = new SelectList(_db.Parents, "ParentId", "ParentName");
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Child child, int ParentId)
+    public ActionResult Create(Machine machine, int EngineerId)
     {
       // var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       // var currentUser = await _userManager.FindByIdAsync(userId);
       // item.User = currentUser;
-      _db.Childs.Add(child);
-      if (ParentId != 0)
+      _db.Machines.Add(machine);
+      if ((Engineer)Id != 0)
       {
-        _db.ParentChild.Add(new ParentChild() { ParentId = ParentId, ChildId = child.ChildId });
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = Machine.MachineId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -47,45 +47,45 @@ namespace Template.Controllers
 
     public ActionResult Details(int id)
     {
-      var thisChild = _db.Childs
-          .Include(child => child.JoinEntries)
-          .ThenInclude(join => join.Parent)
-          .FirstOrDefault(child => child.ChildId == id);
-      return View(thisChild);
+      var thisMachine = _db.Machines
+          .Include(machine => machine.JoinEntries)
+          .ThenInclude(join => join.Engineer)
+          .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
     }
 
     public ActionResult Edit(int id)
     {
-      var thisChild = _db.Childs.FirstOrDefault(child => child.ChildId == id);
-      ViewBag.ParentId = new SelectList(_db.Parents, "ParentId", "ParentName"); // ViewBag only transfers data from controller to view
-      return View(thisChild);
+      var thisMachine = _db.Mahcines.FirstOrDefault(machine => machine.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName"); // ViewBag only transfers data from controller to view
+      return View(thisMachine);
     }
     
     [HttpPost]
-    public ActionResult Edit(Child child, int ParentId)
+    public ActionResult Edit(Machine machine, int EngineerId)
     {
-      if (ParentId != 0)
+      if (EngineerId != 0)
       {
-        _db.ParentChild.Add(new ParentChild() { ParentId = ParentId, ChildId = child.ChildId });
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId });
       }
-      _db.Entry(child).State=EntityState.Modified;
+      _db.Entry(machine).State=EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
-    public ActionResult AddParent(int id)
+    public ActionResult AddEngineer(int id)
     {
-      var thisChild = _db.Childs.FirstOrDefault(childs => childs.ChildId == id);
-      ViewBag.ParentId = new SelectList(_db.Parents, "ParentId", "ParentName");
-      return View(thisChild);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+      return View(thisMachine);
     }
     
     [HttpPost]
-    public ActionResult AddParent(Child child, int ParentId)
+    public ActionResult AddEngineer(Machine machine, int EngineerId)
     {
-      if(ParentId != 0)
+      if(EngineerId != 0)
       {
-        _db.ParentChild.Add(new ParentChild() { ParentId = ParentId, ChildId = child.ChildId});
+        _db.EngineerMachine.Add(new EngineerMachine() { EngineerId = EngineerId, MachineId = machine.MachineId});
       }
         _db.SaveChanges();
         return RedirectToAction("Index");
@@ -93,24 +93,24 @@ namespace Template.Controllers
 
     public ActionResult Delete(int id)
     {
-      var thisChild = _db.Childs.FirstOrDefault(childs => childs.ChildId == id);
-      return View(thisChild);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+      return View(thisMachine);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisChild = _db.Childs.FirstOrDefault(childs => childs.ChildId == id);
-      _db.Childs.Remove(thisChild);
+      var thisMachine = _db.Machines.FirstOrDefault(machines => machines.MachineId == id);
+      _db.Machines.Remove(thisMachine);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
   
     [HttpPost]
-    public ActionResult DeleteParent(int joinId)
+    public ActionResult DeleteEngineer(int joinId)
     {
-      var joinEntry = _db.ParentChild.FirstOrDefault(entry => entry.ParentChildId == joinId);
-      _db.ParentChild.Remove(joinEntry);
+      var joinEntry = _db.EngineerMachine.FirstOrDefault(entry => entry.EngineerMachineId == joinId);
+      _db.EngineerMachine.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
